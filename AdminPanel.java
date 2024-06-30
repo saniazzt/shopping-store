6f26942723af0d61525b9164b5a566a904b1743e
Sania Ezzati <70896572+saniazzt@users.noreply.github.com>
2024-06-30 12:56:36 +0330

    [feature] Rating
    * [feature] Rating the items and sorting by rate
    
    * [feature] viewing scores, sorting and searching
    
    * [feature] score variable added to class
    
    * Update items.txt

--- AdminPanel.java
+++ AdminPanel.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class AdminPanel extends JFrame {
     private ArrayList<Product> items;
@@ -11,47 +13,31 @@ public class AdminPanel extends JFrame {
 
     public AdminPanel() {
         setTitle("پنل مدیریت");
-        setSize(800, 400);
-        setLayout(new GridLayout(5,1));
+        setSize(700, 400);
+        setLayout(new GridLayout(5,3));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         items = loadItemsFromFile();
-        shoppingCartPanel = new JPanel();
+        shoppingCartPanel = new JPanel(new GridLayout(0, 3));
+        displayItems();
         add(shoppingCartPanel, BorderLayout.CENTER);
 
         newItemNameField = new JTextField(20);
         newItemPriceField =  new JTextField(20);
 
         JButton addItemButton = new JButton("افزودن محصول");
+        JButton searchButton = new JButton("جستجو");
+        JButton sortPriceButton = new JButton("مرتب سازی بر اساس قیمت");
+        JButton sortScoreButton = new JButton("مرتب سازی بر اساس امتیاز");
         JPanel addNewItemPanel = new JPanel();
-
-        for (Product item : items) {
-            JLabel itemLabel = new JLabel(item.getName());
-            JButton removeFromCartBtn = new JButton("حذف از فروشگاه");
-
-            removeFromCartBtn.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    items.remove(item);
-                    shoppingCartPanel.remove(itemLabel);
-                    shoppingCartPanel.remove(removeFromCartBtn);
-                    shoppingCartPanel.revalidate();
-                    shoppingCartPanel.repaint();
-                    JOptionPane.showMessageDialog(null, "محصول " + item + " از سبد خرید حذف شد.");
-                }
-            });
-
-            shoppingCartPanel.add(itemLabel);
-            shoppingCartPanel.add(removeFromCartBtn);
-        }
 
         addItemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Product newProduct;
                 newProduct = new Product(newItemNameField.getText()+"  "+newItemPriceField.getText()+"t",
-                                                Integer.valueOf(newItemPriceField.getText()));
+                                                Integer.valueOf(newItemPriceField.getText()),0,0);
                 if (newProduct!=null) {
 
                     items.add(newProduct);
@@ -83,11 +69,74 @@ public class AdminPanel extends JFrame {
             }
         });
 
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String searchItem = JOptionPane.showInputDialog("لطفا نام محصول را وارد کنید:");
+                boolean found = false;
+                String foundedList = "";
+                for (Product item : items) {
+                    if (item.getName().contains(searchItem)) {
+                        found = true;
+                        foundedList += item.getName() + "\n";
+                    }
+                }
+                if (found) {
+                    JOptionPane.showMessageDialog(null, "محصول " + searchItem + " در فروشگاه موجود است." + "\nنتایچ جستجو: " + foundedList);
+                } else {
+                    JOptionPane.showMessageDialog(null, "محصول " + searchItem + " در فروشگاه موجود نیست.");
+                }
+            }
+        });
+
+        sortPriceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Collections.sort(items, new Comparator<Product>() {
+                    @Override
+                    public int compare(Product p1, Product p2) {
+                        return Integer.compare(p1.getPrice(), p2.getPrice());
+                    }
+                });
+                
+                // بازسازی نمایش کالاها بر روی صفحه
+                shoppingCartPanel.removeAll();
+
+                displayItems();
+                
+                shoppingCartPanel.revalidate();
+                shoppingCartPanel.repaint();
+            }
+        });
+
+        sortScoreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Collections.sort(items, new Comparator<Product>() {
+                    @Override
+                    public int compare(Product p1, Product p2) {
+                        return Integer.compare((int)p1.getScore(), (int)p2.getScore());
+                    }
+                });
+                
+                // بازسازی نمایش کالاها بر روی صفحه
+                shoppingCartPanel.removeAll();
+
+                displayItems();
+                
+                shoppingCartPanel.revalidate();
+                shoppingCartPanel.repaint();
+            }
+        });
+
         addNewItemPanel.add(new JLabel("نام محصول:"));
         addNewItemPanel.add(newItemNameField);
         addNewItemPanel.add(new JLabel("قیمت محصول:"));
         addNewItemPanel.add(newItemPriceField);
         addNewItemPanel.add(addItemButton);
+        addNewItemPanel.add(searchButton);
+        addNewItemPanel.add(sortPriceButton);
+        addNewItemPanel.add(sortScoreButton);
 
         add(addNewItemPanel, BorderLayout.SOUTH);
 
@@ -101,11 +150,41 @@ public class AdminPanel extends JFrame {
         setVisible(true);
     }
 
+    public void displayItems(){
+
+        for (Product item : items) {
+            JLabel itemLabel = new JLabel(item.getName());
+            JLabel itemScore = new JLabel();
+            JButton removeFromCartBtn = new JButton("حذف از فروشگاه");
+
+            if (item.getScore() != 0.0)
+                itemScore.setText(String.valueOf(item.getScore()));
+            else
+                itemScore.setText("بدون امتیاز");
+
+            removeFromCartBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    items.remove(item);
+                    shoppingCartPanel.remove(itemLabel);
+                    shoppingCartPanel.remove(removeFromCartBtn);
+                    shoppingCartPanel.revalidate();
+                    shoppingCartPanel.repaint();
+                    JOptionPane.showMessageDialog(null, "محصول " + item.getName() + " از فروشگاه حذف شد.");
+                }
+            });
+
+            shoppingCartPanel.add(itemLabel);
+            shoppingCartPanel.add(itemScore);
+            shoppingCartPanel.add(removeFromCartBtn);
+        }
+    }
+
     public void saveItemsToFile(ArrayList<Product> items) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("items.txt"));
             for (Product item : items) {
-                writer.write(item.getName()+";"+item.getPrice()+"\n");
+                writer.write( item.getName() +";"+ item.getPrice() +";"+ item.getScore() +";"+ item.getScoredNum() +"\n");
             }
             writer.close();
 
@@ -122,7 +201,7 @@ public class AdminPanel extends JFrame {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] info = line.split(";");
-                Product p = new Product(info[0],Integer.valueOf(info[1]));
+                Product p = new Product(info[0],Integer.valueOf(info[1]),Float.valueOf(info[2]),Integer.valueOf(info[3]));
                 items.add(p);
             }
             reader.close();
