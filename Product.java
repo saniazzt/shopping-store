ba1270ea0294bc9fe5139354a3bac56f4d86d535
Sania Ezzati <70896572+saniazzt@users.noreply.github.com>
2024-06-30 17:26:04 +0330

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,15 +1,15 @@
-package shop;
-
 public class Product {
-
 
     private int price;
     private String name;
+    private float score;
+    private int scoredNum;
 
-
-    public Product(String name, int price) {
+    public Product(String name, int price, float score, int scoredNum) {
         this.name = name;
         this.price = price;
+        this.score = score;
+        this.scoredNum = scoredNum;
     }
 
     public String getName() {
@@ -26,5 +26,21 @@ public class Product {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public int getScoredNum(){
+        return scoredNum;
+    }
+
+    public void setScoredNum(int scoredNum) {
+        this.scoredNum = scoredNum;
     }
 }
